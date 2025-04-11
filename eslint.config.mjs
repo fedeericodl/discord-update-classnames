19b0c159e9b972dc41bb52430403bc503d48ef7e
Federico <38290480+fedeericodl@users.noreply.github.com>
2025-04-11 17:01:51 +0200

    chore: update deps and lint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,13 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
+import eslint from "@eslint/js";
 import eslintConfigPrettier from "eslint-config-prettier";
+import globals from "globals";
+import tseslint from "typescript-eslint";
 
-/** @type {import('eslint').Linter.Config[]} */
-export default [
+export default tseslint.config(
+    eslint.configs.recommended,
+    tseslint.configs.recommended,
+    eslintConfigPrettier,
     { ignores: ["**/data/*", "**/dist/*", "**/lib/*"] },
     { files: ["**/*.{js,mjs,cjs,ts}"] },
     { languageOptions: { globals: globals.node } },
-    pluginJs.configs.recommended,
-    ...tseslint.configs.recommended,
-    eslintConfigPrettier,
-];
+);
